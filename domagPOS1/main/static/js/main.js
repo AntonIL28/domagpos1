b8350a4d7001ca4e48ed875d16b2649beadb0aac
AntonIL28 <ai2335102@gmail.com>
2024-03-09 17:12:04 -0800

    Intento 1 peticiones ajax en CxC

--- domagPOS1/main/static/js/main.js
+++ domagPOS1/main/static/js/main.js
@@ -13,35 +13,10 @@ function toggleDiv_clientes() {
 }
 
 /*Funcion de prueba para recoger datos*/
-function ajax() {
-    const http = new XMLHttpRequest();
-    const url = "http://127.0.0.1:8000/ajax-view/";
+function MostrarMensaje(){
 
-    http.onreadystatechange = function (){
-        if(this.readyState == 4 && this.status == 200){
-            console.log(this.responseText);
-            document.getElementById("response").innerHTML =this.responseText;
-        }
-    }
+	$.get('funciones.html',function(mensaje,estado){
 
-    http.open("GET", url);
-    http.send();
-    }
-
-document.getElementById("boton").addEventListener("click", function(){
-    ajax();
-});
-
-
-/*Barra de busqueda dinamica
-const onSearch = () =>{
-    const input =document.querySelector("#search");
-    const filter =input.ariaValueMax.toUpperCase();
-    const list = document.querySelectorAll("#dynamic-table td")
-    list.forEach((el)=>{
-        const text =el.textContent.toUpperCase();
-
-        el.style.display =text.includes(filter) ? "": "none";
-    });
-};*/ //Codigo de prueba (No funciono)
-
+		document.getElementById('resultado').innerHTML=mensaje;
+	})
+}
